5d45ce88b0a8ac87b71d8d85c28d12d34aadfe31
Jordan Hauser <jordan@jhauser.com>
2019-02-16 18:41:39 -0500

    update: setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 from setuptools import setup, find_packages
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'plate/README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='Plate',
-    version='0.1.0',
+    version='0.1.2',
     description='CLI tool to create and use templates',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jordan Hauser',
     author_email='jordan@jhauser.com',
-    url='',
     license='MIT',
+    url='https://github.com/j0rdnh/plate',
     packages=find_packages(),
     package_data={
         '': ['*.md', '*.cfg']
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
     install_requires=[
         'Click'
     ],
